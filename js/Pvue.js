99ab43caa775684e12fe555dee24c73d83afe2dd
panyongxu <panyx_1002@163.com>
2019-03-21 00:08:36 +0800

    第一次提交

---
+++ js/Pvue.js
@@ -0,0 +1,44 @@
+// Vue响应式的原理deﬁneProperty 
+// 使用 new PVue({...})
+
+class PVue {
+    constructor(options) {
+        // 整个配置对象保存到实例
+        this.$options = options
+        this.$data = options.data
+        // 给 $data 所有数据进行绑定代理
+        this.observer(this.$data)
+    }
+    // 观察者
+    observer(data) {
+        // 当 data 存在且为对象时
+        if (!data || typeof data != 'object') {
+            return
+        }
+        Object.keys(data).map(key => {
+            this.defineReactive(data, key, data[key])
+        })
+    }
+    // 添加 deﬁneProperty  
+    defineReactive(data, key, value) {
+        console.log(typeof value);
+        
+        var vm = this;
+        Object.defineProperty(data, key, {
+            get() {
+                return value
+            },
+            set(newVal) {
+                console.log(value);
+                // data[key] = newVal
+                value = newVal
+                console.log(vm.$data);
+                // this.data[key] = newVal;
+                console.log(`${key} 被更新了`);
+                console.log(`newVal: ${newVal}`);
+                console.log(data)
+                
+            }
+        })
+    }
+}
